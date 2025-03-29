26419869aa5bc9b792c74384dec13c58bd31cc81
naresh <Naresh@gmail.com>
2025-03-29 13:38:34 +0530

    multi ec2

--- ec2-creation/main.tf
+++ ec2-creation/main.tf
@@ -2,5 +2,29 @@ resource "aws_instance" "expense-frontend" {
   ami                     = "ami-0fcc78c828f981df2"
   instance_type           = "t3.micro"
   vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+
+   tags = {
+    Name = "frontend-dev"
+  }
+  
+}
+resource "aws_instance" "expense-mysql" {
+  ami                     = "ami-0fcc78c828f981df2"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+
+   tags = {
+    Name = "mysql-dev"
+  }
+  
+}
+resource "aws_instance" "expense-backend" {
+  ami                     = "ami-0fcc78c828f981df2"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+
+   tags = {
+    Name = "backend-dev"
+  }
   
 }
