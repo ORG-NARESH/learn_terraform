f4fd246d276ccc52530e1f9c2f282757c28e0904
naresh <Naresh@gmail.com>
2025-04-02 16:24:32 +0530

    terraform

--- ec2-creation/prod.tf
+++ ec2-creation/prod.tf
@@ -1,30 +1,30 @@
-variable "env" {
-  default = "prod"
-}
-variable "component" {
-  default = {
-    frontend = {
+# variable "env" {
+#   default = "prod"
+# }
+# variable "component" {
+#   default = {
+#     frontend = {
 
-      instance_type          = "t3.micro"
-      ami                    = "ami-0fcc78c828f981df2"
-      vpc_security_group_ids = ["sg-02efeff1df99019a6"]
-      zone_id                = "Z0135212GWRZC8NQ42PM"
+#       instance_type          = "t3.micro"
+#       ami                    = "ami-0fcc78c828f981df2"
+#       vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+#       zone_id                = "Z0135212GWRZC8NQ42PM"
 
-    }
-    mysql = {
+#     }
+#     mysql = {
 
-      instance_type          = "t3.medium"
-      ami                    = "ami-0fcc78c828f981df2"
-      vpc_security_group_ids = ["sg-02efeff1df99019a6"]
-      zone_id                = "Z0135212GWRZC8NQ42PM"
-    }
-    backend = {
+#       instance_type          = "t3.medium"
+#       ami                    = "ami-0fcc78c828f981df2"
+#       vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+#       zone_id                = "Z0135212GWRZC8NQ42PM"
+#     }
+#     backend = {
 
-      instance_type          = "t3.small"
-      ami                    = "ami-0fcc78c828f981df2"
-      vpc_security_group_ids = ["sg-02efeff1df99019a6"]
-      zone_id                = "Z0135212GWRZC8NQ42PM"
-    }
+#       instance_type          = "t3.small"
+#       ami                    = "ami-0fcc78c828f981df2"
+#       vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+#       zone_id                = "Z0135212GWRZC8NQ42PM"
+#     }
 
-  }
-}
+#   }
+# }
