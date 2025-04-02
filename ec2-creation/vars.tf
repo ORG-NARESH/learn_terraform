82e71ce27e98070d122af3086be4974f29e2a1a4
naresh <Naresh@gmail.com>
2025-04-02 13:49:30 +0530

    terraform

--- ec2-creation/vars.tf
+++ ec2-creation/vars.tf
@@ -16,12 +16,14 @@ variable "component" {
       instance_type          = "t3.medium"
       ami                    = "ami-0fcc78c828f981df2"
       vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+      zone_id                = "Z0135212GWRZC8NQ42PM"
     }
     backend = {
 
       instance_type          = "t3.small"
       ami                    = "ami-0fcc78c828f981df2"
       vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+      zone_id                = "Z0135212GWRZC8NQ42PM"
     }
 
   }
