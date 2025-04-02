e4b50a2a07bf004ae1f2fad1b2055d2be3e511b4
naresh <Naresh@gmail.com>
2025-04-02 13:07:45 +0530

    terraform

--- ec2-creation/main.tf
+++ ec2-creation/main.tf
@@ -2,40 +2,21 @@ resource "aws_instance" "main" {
   for_each               = var.component
   ami                    = each.value["ami"]
   instance_type          = each.value["instance_type"]
-  vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+  vpc_security_group_ids = each.value["vpc_security_group_ids"]
 
   tags = {
-    Name = each.key
+    Name = "${each.key}-${env}"
   }
 }
-# resource "aws_instance" "expense-mysql" {
-#   ami                     = "ami-0fcc78c828f981df2"
-#   instance_type           = "t3.micro"
-#   vpc_security_group_ids = ["sg-02efeff1df99019a6"]
 
-#    tags = {
-#     Name = "mysql-dev"
-#   }
 
-# }
-# resource "aws_instance" "expense-backend" {
-#   ami                     = "ami-0fcc78c828f981df2"
-#   instance_type           = "t3.micro"
-#   vpc_security_group_ids = ["sg-02efeff1df99019a6"]
-
-#    tags = {
-#     Name = "backend-dev"
-#   }
-
-# }
-
-# resource "aws_route53_record" "mysql_record" {
-#   zone_id = "Z0135212GWRZC8NQ42PM"
-#   name    = "mysql-dev.eternallearnings.shop"
-#   type    = "A"
-#   ttl     = "10"
-#   records = [aws_instance.expense-mysql.private_ip]
-# }
+resource "aws_route53_record" "main_dns" {
+  zone_id = "Z0135212GWRZC8NQ42PM"
+  name    = "${each.key}-${env}.eternallearnings.shop"
+  type    = "A"
+  ttl     = "10"
+  records = [aws_instance.main.private_ip]
+}
 
 
 # resource "aws_route53_record" "backend_record" {
--- ec2-creation/vars.tf
+++ ec2-creation/vars.tf
@@ -4,20 +4,23 @@ variable "env" {
 variable "component" {
   default = {
     frontend = {
-      Name          = "frontend"
-      instance_type = "t3.micro"
-      ami           = "ami-0fcc78c828f981df2"
+
+      instance_type          = "t3.micro"
+      ami                    = "ami-0fcc78c828f981df2"
+      vpc_security_group_ids = ["sg-02efeff1df99019a6"]
 
     }
     mysql = {
-      Name          = "mysql"
-      instance_type = "t3.medium"
-      ami           = "ami-0fcc78c828f981df2"
+
+      instance_type          = "t3.medium"
+      ami                    = "ami-0fcc78c828f981df2"
+      vpc_security_group_ids = ["sg-02efeff1df99019a6"]
     }
     backend = {
-      Name          = "backend"
-      instance_type = "t3.small"
-      ami           = "ami-0fcc78c828f981df2"
+
+      instance_type          = "t3.small"
+      ami                    = "ami-0fcc78c828f981df2"
+      vpc_security_group_ids = ["sg-02efeff1df99019a6"]
     }
 
   }
