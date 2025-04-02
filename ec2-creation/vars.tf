dc0c6b9155f573c0257fa9d2e8ca619504310ae3
naresh <Naresh@gmail.com>
2025-04-02 12:45:52 +0530

    terraform

--- ec2-creation/vars.tf
+++ ec2-creation/vars.tf
@@ -4,14 +4,16 @@ variable "env" {
 variable "component" {
   default = {
     frontend = {
-      Name          = "frontend"
-      instance_type = "t3.micro"
-      ami           = "ami-0fcc78c828f981df2"
+      Name                   = "frontend"
+      instance_type          = "t3.micro"
+      ami                    = "ami-0fcc78c828f981df2"
+      vpc_security_group_ids = ["sg-02efeff1df99019a6"]
     }
     mysql = {
-      Name          = "mysql"
-      instance_type = "t3.medium"
-      ami           = "ami-0fcc78c828f981df2"
+      Name                   = "mysql"
+      instance_type          = "t3.medium"
+      ami                    = "ami-0fcc78c828f981df2"
+      vpc_security_group_ids = ["sg-02efeff1df99019a6"]
     }
     backend = {
       Name          = "backend"
