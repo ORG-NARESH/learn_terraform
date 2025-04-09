84caa1563e4fd4662176befe7723a6ec69513a67
naresh <Naresh@gmail.com>
2025-04-09 21:15:57 +0530

    created module

--- dev/main.tf
+++ dev/main.tf
@@ -9,3 +9,25 @@ module "frontend"{
     instance_type = var.instance_type
 
 }
+module "mysql"{
+    source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
+    ami = var.ami
+    env = "dev" 
+    vpc_security_group_ids = var.vpc_security_group_ids
+    Name = "mysql"
+    zone_id = var.zone_id
+    domain = var.domain
+    instance_type = var.instance_type
+
+}
+module "backend"{
+    source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
+    ami = var.ami
+    env = "dev" 
+    vpc_security_group_ids = var.vpc_security_group_ids
+    Name = "backend"
+    zone_id = var.zone_id
+    domain = var.domain
+    instance_type = var.instance_type
+
+}
