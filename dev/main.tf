5d5d43132b05d4f3c7d3aa410b3c0b1454c95460
naresh <Naresh@gmail.com>
2025-04-09 19:23:18 +0530

    created module and connect

---
+++ dev/main.tf
@@ -0,0 +1,11 @@
+module "frontend"{
+    source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
+    ami = var.ami
+    env = "dev"
+    vpc_security_group_ids = var.vpc_security_group_ids
+    Name = "frontend"
+    zone_id = var.zone_id
+    domain = var.domain
+    instance_type = var.instance_type
+
+}
---
+++ dev/variables.tf
@@ -0,0 +1,18 @@
+variable "ami" {
+    default = "ami-0fcc78c828f981df2"
+}
+variable "instance_type" {
+    default = "t3.micro"
+}
+ 
+variable "vpc_security_group_ids" {
+    default = "sg-02efeff1df99019a6"
+}
+
+variable "zone_id" {
+    default = "Z0135212GWRZC8NQ42PM"
+}
+
+variable "domain" {
+    default = "eternallearnings.shop"
+}
