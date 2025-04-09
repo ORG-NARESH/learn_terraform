a1fbf7fa3aacf6d6b5a111390df66d892e67319b
naresh <Naresh@gmail.com>
2025-04-09 20:10:22 +0530

    created module

--- dev/variables.tf
+++ dev/variables.tf
@@ -5,10 +5,10 @@ variable "instance_type" {
     default = "t3.micro"
 }
  
-# variable "vpc_security_group_ids" {
-#      type = list(string)
-#     default = ["sg-02efeff1df99019a6"]
-# }
+variable "vpc_security_group_ids" {
+     type = list(string)
+    default = ["sg-02efeff1df99019a6"]
+}
 
 variable "zone_id" {
     default = "Z0135212GWRZC8NQ42PM"
