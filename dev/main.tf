4988999dd72c8788d858298a24f3b8878544628b
naresh <Naresh@gmail.com>
2025-04-09 19:35:52 +0530

    created module and connect

--- dev/main.tf
+++ dev/main.tf
@@ -1,8 +1,8 @@
 module "frontend"{
     source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
     ami = var.ami
-    env = "dev"
-    vpc_security_group_ids = var.vpc_security_group_ids
+    env = "dev" 
+    vpc_security_group_ids = [var.vpc_security_group_ids]
     Name = "frontend"
     zone_id = var.zone_id
     domain = var.domain
